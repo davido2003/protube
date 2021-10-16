5b8f2ddaaffec68a86a85bf9e817e0179842814d
Davido <70234277+davido2003@users.noreply.github.com>
2021-10-16 01:26:38 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-fetch('https://programmingnewsapi.herokuapp.com/?format=json').then((data)=> {
+fetch('https://fakestoreapi.com/products').then((data)=> {
     return data.json();
 
 }).then((completedata)=>{
@@ -6,14 +6,11 @@ fetch('https://programmingnewsapi.herokuapp.com/?format=json').then((data)=> {
     completedata.map((values)=>{
         data1+= `  <div class="card" style="background-color:white;">
         <h1 class="title" style="color:black;" >${values.title}</h1>
-       
-        <video width="500" controls>
-  <source src=${values.video} type="video/mp4">
-  
-</video>
-      <button>
-    <a href=${values.link}>LINK</a>
-      </button>  
+      
+  <img src=${values.image} alt="img"class="images">
+ <p class="category">${values.category}</p>
+  <p class="price">${values.prices}</p>
+ 
         </div>`
     
     });
