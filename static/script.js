b42afc4ddfe1c1ecb8867920a7866d26c60f5a2e
Davido <70234277+davido2003@users.noreply.github.com>
2021-10-19 16:18:24 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-fetch('https://programmingnewsapi.herokuapp.com/?format=json').then((data)=> {
+fetch('http://fakestoreapi.com/products').then((data)=> {
     return data.json();
 
 }).then((completedata)=>{
